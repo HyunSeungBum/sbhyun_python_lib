6d9046832d33b62987f29399df6ac65a9208e65d
Hyun Seung Bum <orion203@gmail.com>
2012-04-02 21:35:27 +0900

    Mysql Database Object complete

--- DbMysql.py
+++ DbMysql.py
@@ -3,7 +3,13 @@
 
 ''' MySQL Database Object '''
 
+__author__ = "Seung-Bum Hyun <orion203@gmail.com>"
+__date__ = "2 April 2012"
+__version__ = "1.0"
+__License__ = "GPL"
+
 import os
+import sys
 
 try:
         import MySQLdb
@@ -11,20 +17,22 @@ except ImportError, e:
         raise ImportError("%s" % e)
 
 try:
-        from sbhyun_utils import log
+        sys.path.append(os.getcwd())
+        from sbhyun_utils import logs
         from DbCfgParser import dbCfgParser
-        from DbInterface import DbInterface
 except ImportError, e:
         raise ImportError("%s" % e)
 
 
-class dbMysql(dbCfgParser, log) implements DbInterface:
+class dbMysql(dbCfgParser):
 
-        def __init__(self, host):
-                self.log = log()
+        def __init__(self, host=None):
+                self._log = logs()
 
-                self.setParam(host)
-                self._serverCfg = self.getConfig()
+                if host is not None:
+                        self.setParam(host)
+                        self._serverCfg = self.getConfig()
+                        self.dbConnect()
 
         def dbConnect(self, host=None, dbName=None, userId=None, passwd=None):
                 if host is None:
@@ -42,7 +50,7 @@ class dbMysql(dbCfgParser, log) implements DbInterface:
                 try:
                         self._db = MySQLdb.connect(host=host, user=userId, passwd=passwd, db=dbName)
                 except Exception, e:
-                        raise self.log.error('%s' % str(e))
+                        raise self._log.error('%s' % str(e))
 
                 self._cursor = self._db.cursor()
                 self._DictCursor = MySQLdb.cursors.DictCursor(self._db)
@@ -54,9 +62,10 @@ class dbMysql(dbCfgParser, log) implements DbInterface:
 
         def query(self, sql):
                 try:
+                        sql = str(MySQLdb.escape_string(sql))
                         self._cursor.execute(sql)
                 except Exception, e:
-                        self.log.error('%s, query: ' % (str(e), sql))
+                        self._log.error('%s, query: ' % (str(e), sql))
 
         def dbInsert(self, sql):
                 self.query(sql)
@@ -80,3 +89,14 @@ class dbMysql(dbCfgParser, log) implements DbInterface:
                 result = self._DictCursor.fetchall()
                 return result
 
+if __name__ == '__main__':
+        ob = dbMysql('localhost')
+        ob.query('use mysql')
+        sql = 'SELECT * FROM user'
+        results = ob.dbList(sql)
+        for result in results:
+                print result
+        sql = 'SELECT * FROM user LIMIT 1'
+        result = ob.dbSelect(sql)
+        print result
+        ob.dbClose()
